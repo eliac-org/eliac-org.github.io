34ba0c9c977936f2cd4727b93609a63843c9dfde
Juan Manuel <juanmanuelfloresdelacruz@gmail.com>
2025-09-14 16:41:04 +0200

    simplificando la estructura

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://github.com/eliac-org/eliac-org.github.io/',
+  base: '/eliac-front/',  //
+});
